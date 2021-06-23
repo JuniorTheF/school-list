205efbfdf6221fdf2a488ff250705611df37e920
JuniorTheF <fsmart2006@gmail.com>
2021-06-23 14:44:20 +0300

    step 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Table } from 'semantic-ui-react'
 import { getList } from './getList'
+
 
 function App() {
   const [data, setData] = useState([]);
@@ -26,10 +28,26 @@ function App() {
   return (
     <div className="App">
       <ul>
-        {/* {data && data.length > 0 && data.map((item) => <li key={item._id}>{item}</li>)} */}
-        {
-          data && data.length > 0 && data.map((item) => <div key={item.id}> {item.years}</div>)
-        }
+        {/* {
+          data && data.length > 0 && data.map((item) => <div key={item.id}> {item.years}</div>) 
+        } */}
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Название ВУЗа</Table.HeaderCell>
+              <Table.HeaderCell>Направление</Table.HeaderCell>
+              <Table.HeaderCell>Цена обучения</Table.HeaderCell>
+              <Table.HeaderCell>Время обучения</Table.HeaderCell>
+              <Table.HeaderCell>Баллы</Table.HeaderCell>
+              <Table.HeaderCell>Предметы ЕГЭ</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+          <Table.Row>
+            {data.map(item => <Table.Cell>{item.name}</Table.Cell>)}
+          </Table.Row>
+          </Table.Body>
+        </Table>
       </ul>
     </div>
   );
